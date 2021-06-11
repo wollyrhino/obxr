6574947e0fc2f651e453e2f927db0ace5d369822
Wolly Rhino <53662457+wollyrhino@users.noreply.github.com>
2021-06-11 08:34:28 -0500

    Add files via upload

---
+++ pages/ride.js
@@ -0,0 +1,25 @@
+var Ride = { 
+	template: `<div id="page_container">
+	<div id="headline_box">
+	<h1>Delivery Rider</h1>
+	<strong>Delivery Riders Wanted [Obax Scooter Fleet]</strong>
+	<p>We’re searching for delivery personnel with scooter riding experience. Currently, we have routes available for South City, The Hill, Afton and CWE.</p>
+
+	</div>
+
+	<h1 id="headline">Delivery Rider Benefits: </h1><br>
+	<ul id="read_text">
+	<li><h3>Flat rate commission per delivery with peak hour bonus.</h3> </li><br><br>
+	<li>Free or Cheap scooter rental; make 6 or more successful deliveries in a day and you’re scooter rental<br>fee is waived completely, alternatively you’re charged $25 per day, deducted from your weekly pay. </li><br><br>
+	<li>Personal Scooter Assignment: You’re bike, assigned specifically to your Driver Account will be inspected and logged regularly. The cost of repair, tow, and general maintenance is all covered by us. You can check in/out your bike at any time during operation hours. </li><br><br>
+	<li><a href="https://docs.google.com/forms/d/e/1FAIpQLSfKmYjnFwcqMIb7N9LTIGTAvKnYRzv-KpR0lv5oUo3M688pRg/viewform" target="_blank">Submit Application</a></li><br><br>
+	
+
+	</ul>
+	<img width="600" src="images/scooter_fleet_flier_01-tiny.jpg">
+	</div>
+	
+	
+	
+	</div>`
+};
