f10a793ca817a495bcaa4fd34cd53e5f1e468ed7
Wolly Rhino <53662457+wollyrhino@users.noreply.github.com>
2021-06-13 09:47:29 -0500

    Update ride.js

--- pages/ride.js
+++ pages/ride.js
@@ -10,7 +10,7 @@ var Ride = {
 	<h1 id="headline">Delivery Rider Benefits: </h1><br>
 	<ul id="read_text">
 	<li><h3>Flat rate commission per delivery with peak hour bonus.</h3> </li><br><br>
-	<li>Free or Cheap scooter rental; make 6 or more successful deliveries in a day and you’re scooter rental<br>fee is waived completely, alternatively you’re charged $25 per day, deducted from your weekly pay. </li><br><br>
+	<li>Free or Cheap scooter rental; make 6 or more successful deliveries in a day and your scooter rental fee is waived completely, alternatively you’re charged $25 per day, deducted from your weekly pay. </li><br><br>
 	<li>Personal Scooter Assignment: You’re bike, assigned specifically to your Driver Account will be inspected and logged regularly. The cost of repair, tow, and general maintenance is all covered by us. You can check in/out your bike at any time during operation hours. </li><br><br>
 	<li><a href="https://docs.google.com/forms/d/e/1FAIpQLSfKmYjnFwcqMIb7N9LTIGTAvKnYRzv-KpR0lv5oUo3M688pRg/viewform" target="_blank">Submit Application</a></li><br><br>
 	
