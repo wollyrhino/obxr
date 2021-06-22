4bd3c04d0baafcd2dc91f3e677728e4f2f69bdb9
Wolly Rhino <53662457+wollyrhino@users.noreply.github.com>
2021-06-21 21:11:32 -0500

    Add files via upload

--- pages/rentalform.js
+++ pages/rentalform.js
@@ -14,9 +14,22 @@ let Rentalform = {
     width="800px" 
     allowfullscreen></iframe></div>
 
+    <h1>Rent a Scooter</h1><br>
+
+    <div ><iframe src="https://swoopbackup.bubbleapps.io/scooter_rental" 
+    style="border:0px #ffffff none;" 
+    name="myiFrame" 
+    scrolling="no" 
+    frameborder="1" 
+    marginheight="0px" 
+    marginwidth="0px" 
+    height="750px" 
+    width="800px" 
+    allowfullscreen></iframe></div>
+
    </div>
    </div>
-   <button @click="saveContact" >submit</button>
+   <
    </div>
 
     </div>
